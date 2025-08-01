7103b2f8bfedd62d3991567324739ec8c06e9416
Vaso_Tchaniashvili <va.chanishvili@gmail.com>
2025-08-01 12:53:44 +0400

    updated "+" and "-" buttons

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ lambdaGame.innerHTML = `
 let isEnabled = true;
 let autoSpinCount = [];
 let autoSpinCountIndex = -1;
+let betValues = [];
+let betValuesIndex = 0;
 
 const iframe = document.getElementById("gameFrame");
 
@@ -40,6 +42,10 @@ const soundButton = document.getElementById("soundBtn");
 const autoPlayInnerBtns = document.querySelectorAll("#autoPlayContainer .innerBtnContainer .smallBtn");
 const autoPlayPlusBtn = autoPlayInnerBtns[0];
 const autoPlayMinusBtn = autoPlayInnerBtns[1];
+
+const betsInnerBtns = document.querySelectorAll("#betsContainer .innerBtnContainer .smallBtn");
+const betsPlusBtn = betsInnerBtns[0];
+const betsMinusBtn = betsInnerBtns[1];
 
 /* = PostMessages = */
 
@@ -62,16 +68,17 @@ spinButton.addEventListener("click", () => {
 	iframe.contentWindow.postMessage({type: "Spin", data: "Bet"}, "http://localhost:7295");
 });
 
+/* = AutoPlay = */
+
 window.addEventListener("message", ($e) => {
 	if ($e.origin !== "http://localhost:7295") return;
 
 	if ($e.data.type === "AutoSpin") {
 		autoSpinCount = $e.data.data;
 		// console.log("Child sent AutoSpin:", autoSpinCount);
+		autoSpinCount.unshift(0);
 	}
 });
-
-/* = AutoPlay = */
 
 autoPlayButton.addEventListener("click", () => {
 	// console.log("🔼 Auto Play window open Message Sent to Game");
@@ -80,28 +87,53 @@ autoPlayButton.addEventListener("click", () => {
 });
 
 autoPlayPlusBtn.addEventListener("click", () => {
-	if (autoSpinCount.length > 0 && autoSpinCountIndex < autoSpinCount.length - 1) {
-		autoSpinCountIndex = (autoSpinCountIndex + 1) % autoSpinCount.length;
+	if (autoSpinCountIndex < autoSpinCount.length - 1) {
+		autoSpinCountIndex++;
 		const selectedCount = autoSpinCount[autoSpinCountIndex];
-		console.log("Selected auto spin count:", selectedCount);
+		console.log("Selected AutoSpin value:", selectedCount);
 	}
 });
 
 autoPlayMinusBtn.addEventListener("click", () => {
-	if (autoSpinCount.length > 0) {
-		autoSpinCountIndex = Math.max(-1, autoSpinCountIndex - 1);
-		const selectedCount = autoSpinCountIndex === -1 ? 0 : autoSpinCount[autoSpinCountIndex];
-		console.log("Selected auto spin count:", selectedCount);
+	if (autoSpinCountIndex > 1) {
+		autoSpinCountIndex--;
+		const selectedCount = autoSpinCount[autoSpinCountIndex];
+		console.log("Selected AutoSpin value:", selectedCount);
 	}
 });
 
 /* = Bets = */
+
+window.addEventListener("message", ($e) => {
+	if ($e.data.type === "BetValues") {
+		betValues = $e.data.data;
+		console.log("Child sent BetValues:", betValues);
+	}
+});
 
 betsButton.addEventListener("click", () => {
 	// console.log("🔼 Bets window open Message Sent to Game");
 	// console.log("waiting for bet values");
 	iframe.contentWindow.postMessage({type: "BetValues", data: {betValues}}, "http://localhost:7295");
 });
+
+betsPlusBtn.addEventListener("click", () => {
+	if (betValuesIndex < betValues.length - 1) {
+		betValuesIndex++;
+		const selectedCount = betValues[betValuesIndex];
+		console.log("Selected bet value:", selectedCount);
+	}
+});
+
+betsMinusBtn.addEventListener("click", () => {
+	if (betValuesIndex > 0) {
+		betValuesIndex--;
+		const selectedCount = betValues[betValuesIndex];
+		console.log("Selected bet value:", selectedCount);
+	}
+});
+
+/* = Sound = */
 
 soundButton.addEventListener("click", () => {
 	// console.log("🔼 Sound mute/unmute Message Sent to Game");
